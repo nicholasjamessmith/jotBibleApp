99c342764e537c4406e298a6b9d63bf822c85dbe
nicholasjamessmith <njsmith6@gmail.com>
2026-04-12 10:19:38 -0400

    removed AI code, refactored API calls, commented out styles

--- book.js
+++ book.js
@@ -1,8 +1,9 @@
+import { API_key } from './env.js';
 const versionId = 'de4e12af7f28f599-01';
 const bookList = document.getElementById('book-list');
 
 fetch(`https://api.scripture.api.bible/v1/bibles/${versionId}/books`, {
-  headers: { 'api-key': API_KEY }
+  headers: { 'api-key': API_key }
 })
   .then(res => res.json())
   .then(res => {
---
+++ my_key.js
@@ -0,0 +1,2 @@
+//To store API key as variable
+//export const API_KEY = "aa59d22df4b68dde82fc124df85a0a31"

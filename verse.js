0f4a0329444258d3306185d8c5fbb6fd5aa58b09
nicholasjamessmith <njsmith6@gmail.com>
2026-05-14 02:59:56 -0400

    next/prev buttons progress

--- verse.js
+++ verse.js
@@ -17,26 +17,58 @@ const bibleChapterID = getParameterByName('chapter'); // Get chapter ID from URL
 const bibleChapterList = document.querySelector('#chapter-list');
 const verseList = document.getElementById('verse-list'); // Target the correct element
 
-let verseHTML = '';
-
+const CHAPTERSTATE = { chapterID: "" }
+const CONTENTSTATE = { chapterContent: "" }
+const NEXTSTATE = { nextChapter: "" }
+const PREVSTATE = { prevChapter: "" }
 
 getChapterContent(bibleVersionID, bibleChapterID).then((data) => {
-  console.log(data);
   if (!data) return;
-  const content = data.content;
-  const el = document.getElementById('verse-list');
-  if (el) el.innerHTML = content;
+  CONTENTSTATE.chapterContent = data.content;
+  CHAPTERSTATE.chapterID = data.id;
+  NEXTSTATE.nextChapter = data.next.id;
+  PREVSTATE.prevChapter = data.previous.id;
+  render();
 });
+
+const render = () => {
+  const el = document.getElementById('verse-list');
+  if (el) el.innerHTML = CONTENTSTATE.chapterContent;
+}
 
 
 const nextButtonClick = () => {
-  console.log("Next button clicked");
-}
+  getChapterContent(bibleVersionID, NEXTSTATE.nextChapter).then((data) => {
+    console.log(data)
+    if (!data) return;
+    const currentChapter = data.content
+    const el = document.getElementById('verse-list');
+    if (el) el.innerHTML = currentChapter;
+    NEXTSTATE.nextChapter = data.next.id;
+    CONTENTSTATE.chapterContent = data.content;
+    render();
+  });
+};
+
+const prevButtonClick = () => {
+  getChapterContent(bibleVersionID, PREVSTATE.prevChapter).then((data) => {
+    console.log(data);
+    if (!data) return;
+    const currentChapter = data.content
+    const el = document.getElementById('verse-list');
+    if (el) el.innnerHTML = currentChapter;
+    PREVSTATE.prevChapter = data.previous.id
+    CONTENTSTATE.chapterContent = data.content;
+    render();
+  });
+};
 
 document.getElementById("next-btn").addEventListener("click", nextButtonClick);
 
-const prevButtonClick = () => {
-  console.log("Prev. button clicked");
-}
-
 document.getElementById("prev-btn").addEventListener("click", prevButtonClick);
+
+//When user clicks next button, next chapter loads if exists.
+//API call to fetch current chapter data.
+//Return current chapter ID
+//Call API with next chapter ID based on current chapter data
+//Return new/current chapter ID 
